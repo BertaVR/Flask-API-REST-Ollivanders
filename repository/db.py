ea8cfdb8c7c18ded803d35659e9ce48c607e52e3
BertaVR <bverges@cifpfbmoll.eu>
2021-03-17 23:45:10 +0100

    feat: :sparkles: Hecho DB

---
+++ repository/db.py
@@ -0,0 +1,8 @@
+class DB():
+    inventario = [["Conjured Mana Cake", 2, 4],["Aged Brie", 3, 4], ["Sulfuras", 2, 4], ["Backstage Passes", 2, 6]]
+
+    @staticmethod
+    def getItem(name):
+        items = DB.inventario
+        print(items)
+        return [item for item in items if item[0] == name]
