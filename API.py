3ff78b514e0fbecf39db7bad76398237db81405b
BertaVR <bverges@cifpfbmoll.eu>
2021-03-19 17:55:01 +0100

    feat: Ruta items añadido

--- API.py
+++ API.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from controller.items import Items
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +11,9 @@ class WelcomeOllivanders(Resource):
         return {'Hello' : 'Ollivanders'}
 
 api.add_resource(WelcomeOllivanders, '/')
+api.add_resource(Items, '/items/<name>')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
