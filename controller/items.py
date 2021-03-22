d4ab2caa734baf9fd74b5da5cc677b9b3ad921d5
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-03-23 00:51:47 +0100

    remake items/test_item

--- controller/items.py
+++ controller/items.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from services.service import Service
-
+import json
 
 
 class Items(Resource):
 
-    #/item/<name>
+    # /item/<name>
 
     def get(self, name):
-        #curl http://localhost:5000/items/name/"Aged+Brie"
-        return Service.get_item(name), 200 #???
+        # curl http://localhost:5000/items/name/"Aged+Brie"
+        #  El 200 queda comentado porque dificultaba los test y nos ha parecido
+        # prescindible ya que es el código por defecto
+        return Service.get_item(name)  # , 200
