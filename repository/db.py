1c084f4afed17da4944aba44ed71f67fd776bc14
BertaVR <bverges@cifpfbmoll.eu>
2021-03-23 20:28:20 +0100

    fix: :bug: Ahora funciona la petición post /había que eliminar una lista)

--- repository/db.py
+++ repository/db.py
@@ -3,7 +3,7 @@ from domain.types import AgedBrie, NormalItem
 class DB():
     inventario = [["Conjured Mana Cake", 2, 4],["Aged Brie", 3, 4], ["Sulfuras", 2, 4], ["Backstage Passes", 2, 6]]
 
-    objetos = [[AgedBrie("Aged Brie", 3, 4)]]
+    objetos = [AgedBrie("Aged Brie", 3, 4)]
 
     @staticmethod
     def get_item(name):
