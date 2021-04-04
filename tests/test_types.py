15f1a285f1471bb83aadd8e19292a06492e85281
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-04-04 19:45:11 +0200

    test_types checking item_types update

---
+++ tests/test_types.py
@@ -0,0 +1,30 @@
+from domain.types import *
+
+
+def test_Backstage():
+    assert Backstage("backstage_passes", 0, 15).update_quality() == 0
+    assert Backstage("backstage_passes", 4, 15).update_quality() == 18
+    assert Backstage("backstage_passes", 6, 15).update_quality() == 17
+    assert Backstage("backstage_passes", 11, 0).update_quality() == 1
+    assert Backstage("backstage_passes", 0, 50).update_quality() == 0
+
+
+def test_ConjuredItem():
+    assert ConjuredItem("conjured_flask", 15, 20).update_quality() == 18
+    assert ConjuredItem("conjured_flask", -3, 20).update_quality() == 16
+    assert ConjuredItem("conjured_flask", -3, 0).update_quality() == 0
+
+
+def test_NormalItem():
+    assert NormalItem("Elixir of the Mongoose", 15, 20).update_quality() == 19
+    assert NormalItem("Elixir of the Mongoose", -3, 20).update_quality() == 18
+    assert NormalItem("Elixir of the Mongoose", -3, 0).update_quality() == 0
+
+
+def test_AgedBrie():
+    assert AgedBrie("Aged Brie", 15, 20).update_quality() == 21
+    assert AgedBrie("Aged Brie", -2, 20).update_quality() == 22
+    assert AgedBrie("Aged Brie", 0, 20).update_quality() == 21
+    assert AgedBrie("Aged Brie", 0, 50).update_quality() == 50
+    assert AgedBrie("Aged Brie", 4, 50).update_quality() == 50
+    assert AgedBrie("Aged Brie", -6, 50).update_quality() == 50
