b6e22deaa3f5fc06aa984a2239dccffa28adb1ae
BertaVR <bverges@cifpfbmoll.eu>
2021-04-12 22:13:52 +0200

    fix: :bug: contraseña de Berta

--- API.py
+++ API.py
@@ -10,7 +10,7 @@ from services.service import Service
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@localhost/guildedrose"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:pomeranian@localhost/guildedrose"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
