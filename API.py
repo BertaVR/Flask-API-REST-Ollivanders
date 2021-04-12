1725ebd3f97e6f3849485acb7afc7c63d225773c
BertaVR <bverges@cifpfbmoll.eu>
2021-04-12 17:46:22 +0200

    build: Base de datos conectada :))))))))))))))))))))))

--- API.py
+++ API.py
@@ -1,12 +1,30 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import os
+from flask_sqlalchemy import SQLAlchemy
 from controller.items import Items
 from controller.objeto import Objeto
 
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:pomeranian@localhost/guildedrose"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Inventario(db.Model):
+    __tablename__='Inventario'
+    id = db.Column(db.Integer, primary_key=True)
+    object_type = db.Column(db.String(30))
+    name =  db.Column(db.String(30), nullable=False)
+    sell_in =  db.Column(db.Integer, nullable=False)
+    quality =  db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+        
+db.drop_all()
+db.create_all()
 
 class WelcomeOllivanders(Resource):
     def get(self):
